a399cd3a42a96e5e68b40b75a83a073345414cd4
Jeancarloq <jcarlobello297@gmail.com>
2021-11-17 11:03:25 -0600

    Agrege el codigo para la clase grafo

--- src/Grafos/Grafo.java
+++ src/Grafos/Grafo.java
@@ -15,7 +15,11 @@ public class Grafo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JFrame ventana = new JFrame("Grafo");
+        ventana.add(new Lienzo());
+        ventana.setSize(600,600);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setVisible(true);
     }
     
 }
