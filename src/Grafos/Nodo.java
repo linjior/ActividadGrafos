64401d5505255300260d9507a1f0d4e70a5fb7f4
vectorMillan <94257669+vectorMillan@users.noreply.github.com>
2021-11-16 23:06:07 -0600

    Agregue codigo en la clase Nodo

--- src/Grafos/Nodo.java
+++ src/Grafos/Nodo.java
@@ -5,10 +5,49 @@
  */
 package Grafos;
 
+import java.awt.Graphics;
+
 /**
  *
  * @author linji
  */
 public class Nodo {
+    private int x, y;
+    private String nombre; 
+    public static final int d = 60;
+
+    public Nodo(int x, int y, String nombre) {
+        this.x = x;
+        this.y = y;
+        this.nombre = nombre;
+    }
     
+    public void pintar(Graphics g){
+        g.drawOval(this.x - d/2, this.y - d/2, d, d);
+        g.drawString(nombre, x, y);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
